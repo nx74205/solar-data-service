7723465921a635f5998919d32e4dcb2af7e65d04
nx74205 <kl.volmer@gmx.de>
2025-11-12 09:51:26 +0100

    Initial commit

---
+++ src/main/java/de/nx74205/solardataservice/SolarDataServiceApplication.java
@@ -0,0 +1,13 @@
+package de.nx74205.solardataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/nx74205/solardataservice/SolarDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nx74205.solardataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
